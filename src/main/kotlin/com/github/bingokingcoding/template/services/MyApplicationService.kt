08e51dcc050a6918b6cf1ec5a2e40d9cde25ecab
GitHub Action <action@github.com>
2022-06-28 14:37:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/bingokingcoding/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bingokingcoding/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bingokingcoding.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bingokingcoding.template.MyBundle
 
 class MyApplicationService {
 
